f5eeb72b980dbb23128d24eec84106fd92dfcaf8
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-09-07 20:46:24 +0900

    Add utils

---
+++ scripts/dump-l4t-version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eu
+
+L4T=`head -n1 /etc/nv_tegra_release | sed -E "s/# (R[0-9]*) .*REVISION: ([0-9\.]*),.*/\1.\2/g"`
+
+JP="N/A"
+if [ "$L4T" == "R32.2.1" ]; then JP="JetPack4.2.2"; fi
+if [ "$L4T" == "R32.2.3" ]; then JP="JetPack4.2.3"; fi
+if [ "$L4T" == "R32.3.1" ]; then JP="JetPack4.3"; fi
+if [ "$L4T" == "R32.4.3" ]; then JP="JetPack4.4"; fi
+
+echo $L4T, $JP

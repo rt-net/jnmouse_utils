61614c5d9e9cdc920619379dd8be71d18aa668dc
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-12-16 18:00:59 +0900

    Fix 'file not found' error

--- scripts/disable-auto-apt-upgrade.sh
+++ scripts/disable-auto-apt-upgrade.sh
@@ -4,4 +4,8 @@ sudo systemctl mask apt-daily.service
 sudo systemctl mask apt-daily.timer
 sudo systemctl mask apt-daily-upgrade.service
 sudo systemctl mask apt-daily-upgrade.timer
-sudo sed -i -e 's/Update-Package-Lists "1"/Update-Package-Lists "0"/g' -e 's/Unattended-Upgrade "1"/Unattended-Upgrade "0"/g' /etc/apt/apt.conf.d/20auto-upgrades
+for TARGET_FILE in $(grep -ril "Update-Package-Lists" /etc/apt/apt.conf.d/); do
+    echo Rewriting $TARGET_FILE
+    sudo sed -i -e 's/Update-Package-Lists "1"/Update-Package-Lists "0"/g' -e 's/Unattended-Upgrade "1"/Unattended-Upgrade "0"/g' $TARGET_FILE
+done
+
